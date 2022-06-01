b8f3bdf439121a08e71db4fffce4ca056af12e60
Rahul Lohra <rahullohra987@gmail.com>
2022-06-01 09:47:20 +0530

    Some css

--- src/components/LogIn/login.component.jsx
+++ src/components/LogIn/login.component.jsx
@@ -1,8 +1,33 @@
+import './login.style.css'
+import { Fragment } from "react"
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
     return (
-        <h1>This is Sign Up Page</h1>
+        <Fragment>
+            <div className="login-page">
+                <div className="login-container">
+                    <div className="login-inputs">
+                        <form action="submit">
+                            <label for="fname">Email:</label><br/>
+                            <input className='inputs' label="email"
+                                    name="email" />
+                            <label for="fname">Password:</label><br/>
+                            <input className='inputs'  label="Password"
+                                    name="Password" type='password'/>
+                        </form>
+                    </div>
+                    <div className="submit">
+                        <Link className='login-button' to='/'>
+                            <button className='login-button'>Login</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
+
 
 export default Login
